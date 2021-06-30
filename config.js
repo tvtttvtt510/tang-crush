8db0cf43e0fcbd4fc717b017584e844baa11bf8a
tvtttvtt510 <86724335+tvtttvtt510@users.noreply.github.com>
2021-06-30 19:53:58 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,16 @@
 const CONFIG = {
-    introTitle: 'Hi Thảo',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
-    btnReply: 'Gửi cho anh <3',
+    introTitle: 'Hi Hnil',
+    introDesc: `Đã 2 năm chúng mình ngồi với nhau rùii`,
+    btnIntro: 'Ừ ấy',
+    title: 'Dạo này mình dùng shopee hay quá tay 😙',
+    desc: 'Bạn bank cho mình thêm mấy trăm nữa được không ',
+    btnYes: 'Thôi 1 triệu cho tròn b',
+    btnNo: 'Thui :(',
+    question:'Sao b yêu nghèo mà vẫn bank tiền cho mình dị <3',
+    btnReply: 'Gửi <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
-    btnAccept: 'Okiiiii lun <3',
-    messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    mess: 'Mik biết mà 🥰. Yêu b nhiều nhiều 😘😘',
+    messDesc: 'Mai chuyển nóng ngay nhé b.',
+    btnAccept: 'Oke b ơi',
+    messLink: 'https://www.messenger.com/t/100038032850819' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
